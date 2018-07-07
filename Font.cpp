2014b024d2c20073946447d27d9fa95f574569f3
Floros Panagiotis <p.f1936@windowslive.com>
2018-07-07 21:11:38 +0300

    Added support for newline char

--- Font.cpp
+++ Font.cpp
@@ -15,8 +15,15 @@ void Font::RenderText(std::string text, int x, int y)
 {
 	const char *chars = text.c_str();
 	int length = (unsigned)strlen(chars);
+	int screenIndex = 0;
 	for (int i = 0; i < length; i++) {
 		int index = chars[i] - 32;
-		fontSprite->RenderFrame(i*width+x, y, index / 32, index % 32);
+		if (chars[i] == '\n') {
+			screenIndex = 0;
+			y += height;
+		}else {
+			fontSprite->RenderFrame(screenIndex*width + x, y, index / 32, index % 32);
+			screenIndex++;
+		}
 	}
 }
